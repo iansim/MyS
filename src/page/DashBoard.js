ef8461836b4777182aa4a570b771e23c64901e9c
Shen Yang <shenyangziyu@gmail.com>
2018-08-30 23:28:05 +0800

    init

--- src/page/DashBoard.js
+++ src/page/DashBoard.js
@@ -1,10 +1,26 @@
-import React, {Component} from 'react';
-import { Container, Header, Content, ListItem, CheckBox, Text, Body } from 'native-base';
+import React, { Component } from 'react';
+import { Container, Icon, Header, Content, ListItem, CheckBox, Text, Body, Left, Button, Title, Right } from 'native-base';
 
 class DashBoard extends Component {
 
     render() {
-        return (<Container><Text>Haha</Text></Container>);
+        return (<Container>
+            <Header>
+                <Left>
+                    <Button transparent>
+                        <Icon name='arrow-back' />
+                    </Button>
+                </Left>
+                <Body>
+                    <Title>Header</Title>
+                </Body>
+                <Right>
+                    <Button transparent>
+                        <Icon name='menu' />
+                    </Button>
+                </Right>
+            </Header>
+            <Text>Haha</Text></Container>);
     }
 }
 export default DashBoard;
